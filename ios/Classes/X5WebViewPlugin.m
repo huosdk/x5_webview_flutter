2c8305731bc66e8c6518943b4b85d6311d65ede2
张墩钛 <274496856@qq.com>
2019-10-31 11:02:05 +0800

    干掉swift

---
+++ ios/Classes/X5WebViewPlugin.m
@@ -0,0 +1,20 @@
+#import "X5WebViewPlugin.h"
+
+@implementation X5WebViewPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_download_plugin"
+            binaryMessenger:[registrar messenger]];
+  X5WebViewPlugin* instance = [[X5WebViewPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
