710a8708b6aa221f168057e17f7199f6eaeb5c47
张墩钛 <274496856@qq.com>
2019-10-31 10:06:24 +0800

    干掉swift 修改instance

--- ios/Classes/X5WebviewPlugin.m
+++ ios/Classes/X5WebviewPlugin.m
@@ -1,5 +1,8 @@
 #import "X5WebviewPlugin.h"
 
+@interface X5WebviewPlugin : NSObject<FlutterPlugin>
+@end
+
 @implementation X5WebviewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
 
@@ -8,6 +11,7 @@
                     binaryMessenger:[registrar messenger]];
   X5WebviewPlugin* instance = [[X5WebviewPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+
 }
 @end
 
