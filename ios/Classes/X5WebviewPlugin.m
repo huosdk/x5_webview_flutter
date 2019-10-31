78342614150aa81af6b5319e11f078b930c235df
张墩钛 <274496856@qq.com>
2019-10-31 09:31:22 +0800

    干掉swift

--- ios/Classes/X5WebviewPlugin.m
+++ ios/Classes/X5WebviewPlugin.m
@@ -1,6 +1,12 @@
 #import "X5WebviewPlugin.h"
+#import <x5_webview/x5_webview-Swift.h>
 
 @implementation X5WebviewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+
+  FlutterMethodChannel *channel = [FlutterMethodChannel
+              methodChannelWithName:@"x5_webview"
+                    binaryMessenger:[registrar messenger]];
+      [registrar addMethodCallDelegate:instance channel:channel];
 }
 @end
